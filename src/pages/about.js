6ad0cc68904dd599d136f0481171df28ae36990e
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-09-20 21:08:37 +0500

    About section completed

--- src/pages/about.js
+++ src/pages/about.js
@@ -33,7 +33,7 @@ const AnimateNumbers = ({value}) => {
   const ref = useRef(null);
   const motionValue = useMotionValue(0);
   const springValue = useSpring(motionValue, { duration: 3000});
-  const isInView = useInView(ref);
+  const isInView = useInView(ref, {once: true});
 
   useEffect(() => {
     if(isInView) {
