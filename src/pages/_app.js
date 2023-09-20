caeb7d163b7566ec648efd3a539ab0b323713ecd
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2023-09-20 12:40:55 +0500

    Revert "Footer section completed"

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import '../styles/globals.css'
 import { Montserrat } from 'next/font/google'
 import NavBar from '@/components/NavBar'
-import Footer from '@/components/Footer'
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -19,7 +18,6 @@ export default function App({ Component, pageProps }) {
   <main className={`${montserrat.variable} font-sans bg-light w-full min-h-screen`}>
     <NavBar/>
     <Component {...pageProps} />
-    <Footer />
   </main>
     </>
   )
