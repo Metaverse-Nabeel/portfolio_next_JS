98ac64fd174e82acf268a2a66435756c33e0f3d2
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-09-22 09:54:59 +0500

    Applied dark mode to all the components

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -67,14 +67,14 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
         >
           {position}&nbsp;{" "}
           <a
-            className="capitalize text-primary"
+            className="capitalize text-primary dark:text-primaryDark"
             target="_blank"
             href={companyLink}
           >
             @{company}
           </a>
         </h3>
-        <span className="capitalize font-medium text-dark/75">
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
           {time} | {address}
         </span>
         <p className="font-medium w-full">{work}</p>
@@ -97,7 +97,7 @@ const Experience = () => {
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
         />
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           {experiences.map(
