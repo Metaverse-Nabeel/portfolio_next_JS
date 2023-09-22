8b424cf9620dbf05656d3644a068ae9418c4e2aa
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-09-22 09:50:00 +0500

    Created toggle dark mode

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,15 +1,17 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const useThemeSwitcher = () => {
     const preferDarkQuery = "(prefer-color-scheme: dark)";
     const [mode, setMode] = useState("");
+    
     useEffect(() => {
+
       const mediaQuery = window.matchMedia(preferDarkQuery);
       const userPref = window.localStorage.getItem("theme");
 
       const handleChange= () =>{
         if(userPref) {
-            let check = userPref === "dark"? "dark" : "light";
+            let check = userPref === "dark" ? "dark" : "light";
             setMode(check);
             if(check=== "dark") {
                 document.documentElement.classList.add("dark"); 
@@ -21,6 +23,7 @@ const useThemeSwitcher = () => {
         else {
             let check = mediaQuery.matches ? "dark" : "light";
             setMode(check);
+            window.localStorage.setItem("theme", check);
             if(check=== "dark") {
                 document.documentElement.classList.add("dark"); 
             }
@@ -29,6 +32,9 @@ const useThemeSwitcher = () => {
             }
         }
       }
+
+      handleChange();
+
       mediaQuery.addEventListener("change", handleChange);
       
       return () => mediaQuery.removeEventListener("change", handleChange);
@@ -39,7 +45,7 @@ const useThemeSwitcher = () => {
         window.localStorage.setItem("theme", "dark");
         document.documentElement.classList.add("dark"); 
      }
-     else {
+     if(mode === 'light') {
         window.localStorage.setItem("theme", "light");
         document.documentElement.classList.remove("dark"); 
      }
