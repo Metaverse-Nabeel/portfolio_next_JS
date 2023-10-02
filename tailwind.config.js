7f2bc3ec19b5fcc7a143739d64f829391effc30e
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-02 20:02:29 +0500

    Add Mobile Navbar and made it responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: 'class',
-   theme: {
+  darkMode: "class",
+  theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-mont)', ...fontFamily.sans],
+        sans: ["var(--font-mont)", ...fontFamily.sans],
       },
       colors: {
-        dark: '#1B1B1B',
-        light: '#F5F5F5',
-        primary: '#B63E96',
-        primaryDark: '#58E6D9'
+        dark: "#1B1B1B",
+        light: "#F5F5F5",
+        primary: "#B63E96",
+        primaryDark: "#58E6D9",
       },
       animation: {
-        'spin-slow': 'spin 8s linear infinite',
+        "spin-slow": "spin 8s linear infinite",
       },
-      backgroundImage:{
-        circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px);',
-        circularDark: 'repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1B1B1B 8px,#1B1B1B 100px);'
-      }
+      backgroundImage: {
+        circularLight:
+          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px);",
+        circularDark:
+          "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1B1B1B 8px,#1B1B1B 100px);",
+      },
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: "479px" },
+      // => @media (max-width: 479px) { ... }
     },
   },
   plugins: [],
-}
+};
