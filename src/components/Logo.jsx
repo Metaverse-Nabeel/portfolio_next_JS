405f8cf01595dc0f1cc16deed5608c7709b3753e
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-06 23:34:17 +0500

    Update the logo text for dark background

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -9,7 +9,7 @@ const Logo = () => {
     <div className="flex items-center justify-center mt-2">
       <MotionLink
         href="/"
-        className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light"
+        className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light dark:bg-light dark:text-dark"
         whileHover={{
           backgroundColor: [
             "rgba(131,58,180,1)",
